8c74f792f242bd0978f68c542619f8bb11bc11a9
Felipe Catania <felicatania@gmail.com>
2018-01-04 19:00:23 -0800

    Card styling.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -8,7 +8,9 @@ let dummyCards = [
   {title: 'code', exact_translation: 'código', example: 'Yo escribo código.', example_meaning: 'I write code.'},
   {title: 'variable', exact_translation: 'variable', example: 'Definí una variable.', example_meaning: 'I defined a variable.'},
   {title: 'function', exact_translation: 'función', example: 'Esto es una función.', example_meaning: 'This is a function.'},
-  {title: 'constant', exact_translation: 'constante', example: 'No puedo modificar constantes.', example_meaning: 'I can\'t modify constants.'}
+  {title: 'constant', exact_translation: 'constante', example: 'No puedo modificar constantes.', example_meaning: 'I can\'t modify constants.'},
+  {title: 'development', exact_translation: 'desarrollo', example: 'Me gusta el desarrollo web.', example_meaning: 'I like web development.'},
+  {title: 'developer', exact_translation: 'desarrollador', example: 'Soy un desarrollador web.', example_meaning: 'I\'m a web developer.'}
 ];
 
 class App extends React.Component {
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,10 +4,10 @@ import './Card.css';
 const Card = ({ title, exact_translation, example, example_meaning }) => {
   return (
     <div className="Card">
-      <div>{title}</div>
-      <div>{`Exact translation: ${exact_translation}`}</div>
-      <div>{`Example: ${example}`}</div>
-      <div>{`Example meaning: ${example_meaning}`}</div>
+      <div className="Card-title"><b>{title}</b></div>
+      <div className="Card-content"><b>Exact translation: </b>{exact_translation}</div>
+      <div className="Card-content"><b>Example: </b>{example}</div>
+      <div className="Card-content"><b>Example meaning: </b>{example_meaning}</div>
     </div>
   );
 }
