cbd03fda7a7908df2e1e5d52ac02aef9637a0434
Felipe Catania <felicatania@gmail.com>
2018-01-05 00:22:28 -0800

    Implemented basic mongodb database. Server now pulls data from it.

---
+++ server/database/dbschema.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/spanish-padawan');
+
+
+let cardSchema = new Schema({
+  title: String,
+  exact_translation: String,
+  example: String,
+  example_meaning: String
+});
+
+let questionSchema = new Schema({
+  title: String,
+  options: [String],
+  correct_index: Number
+});
+
+let Card = mongoose.model('Card', cardSchema);
+let Question = mongoose.model('Question', questionSchema);
+
+exports.findAllCards = (cb) => {
+  Card.find((err, cards) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, cards);
+    }
+  })
+};
+
+exports.findAllQuestions = (cb) => {
+  Question.find((err, cards) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, cards);
+    }
+  })
+};
+
+exports.createCards = (cards) => {
+  Card.create(cards);
+};
+
+exports.createQuestions = (questions) => {
+  Question.create(questions);
+};
--- server/index.js
+++ server/index.js
@@ -1,36 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const { findAllCards, findAllQuestions, createCards, createQuestions } = require('./database/dbschema.js');
 
 const app = express();
 
-const dummyCards = [
-  {title: 'code', exact_translation: 'código', example: 'Yo escribo código.', example_meaning: 'I write code.'},
-  {title: 'variable', exact_translation: 'variable', example: 'Definí una variable.', example_meaning: 'I defined a variable.'},
-  {title: 'function', exact_translation: 'función', example: 'Esto es una función.', example_meaning: 'This is a function.'},
-  {title: 'constant', exact_translation: 'constante', example: 'No puedo modificar constantes.', example_meaning: 'I can\'t modify constants.'},
-  {title: 'development', exact_translation: 'desarrollo', example: 'Me gusta el desarrollo web.', example_meaning: 'I like web development.'},
-  {title: 'developer', exact_translation: 'desarrollador', example: 'Soy un desarrollador web.', example_meaning: 'I\'m a web developer.'}
-];
+// const dummyCards = [
+//   {title: 'code', exact_translation: 'código', example: 'Yo escribo código.', example_meaning: 'I write code.'},
+//   {title: 'variable', exact_translation: 'variable', example: 'Definí una variable.', example_meaning: 'I defined a variable.'},
+//   {title: 'function', exact_translation: 'función', example: 'Esto es una función.', example_meaning: 'This is a function.'},
+//   {title: 'constant', exact_translation: 'constante', example: 'No puedo modificar constantes.', example_meaning: 'I can\'t modify constants.'},
+//   {title: 'development', exact_translation: 'desarrollo', example: 'Me gusta el desarrollo web.', example_meaning: 'I like web development.'},
+//   {title: 'developer', exact_translation: 'desarrollador', example: 'Soy un desarrollador web.', example_meaning: 'I\'m a web developer.'}
+// ];
 
-const dummyQuestions = [
-  {title: 'How do you say code?', options: ['codego', 'códego', 'codigo', 'código'], correct_index: 3},
-  {title: 'How do you say variable?', options: ['variable', 'váriable', 'varioble', 'varieble'], correct_index: 0},
-  {title: 'How do you say function?', options: ['funcion', 'funciona', 'función', 'funciono'], correct_index: 2},
-  {title: 'How do you say constant?', options: ['constant', 'constanti', 'constanto', 'constante'], correct_index: 3},
-  {title: 'How do you say development?', options: ['desarrollador', 'desrollo', 'desarrollo', 'désrrollo'], correct_index: 2},
-  {title: 'How do you say developer?', options: ['desarrollador', 'desrollodor', 'desarrollo', 'desarrollado'], correct_index: 0}
-];
+// const dummyQuestions = [
+//   {title: 'How do you say code?', options: ['codego', 'códego', 'codigo', 'código'], correct_index: 3},
+//   {title: 'How do you say variable?', options: ['variable', 'váriable', 'varioble', 'varieble'], correct_index: 0},
+//   {title: 'How do you say function?', options: ['funcion', 'funciona', 'función', 'funciono'], correct_index: 2},
+//   {title: 'How do you say constant?', options: ['constant', 'constanti', 'constanto', 'constante'], correct_index: 3},
+//   {title: 'How do you say development?', options: ['desarrollador', 'desrollo', 'desarrollo', 'désrrollo'], correct_index: 2},
+//   {title: 'How do you say developer?', options: ['desarrollador', 'desrollodor', 'desarrollo', 'desarrollado'], correct_index: 0}
+// ];
+// For first time.
+// createCards(dummyCards);
+// createQuestions(dummyQuestions);
 
 app.use(cors());
 
+// TODO: Modify get requests to find in mongodb.
+
 app.get('/api/cards', (req, res) => {
   console.log('getting cards');
-  res.status(200).json(dummyCards);
+  // res.status(200).json(dummyCards);
+  findAllCards((err, cards) => {
+    res.status(200).json(cards);
+  });
 });
 
 app.get('/api/questions', (req, res) => {
   console.log('getting questions');
-  res.status(200).json(dummyQuestions);
+  // res.status(200).json(dummyQuestions);
+  findAllQuestions((err, questions) => {
+    res.status(200).json(questions);
+  })
 });
 
 let port = process.env.PORT || 2410; // for theoretical deployment
