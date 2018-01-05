b22219506a2b4f8988ad1d9fc3caa6982243612a
Felipe Catania <felicatania@gmail.com>
2018-01-04 16:32:44 -0800

    Header component now toggles isShowing state within App.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from '../Header/Header.jsx';
 
-function App(props) {
-  return (
-    <div className="App">
-      <Header />
-    </div>);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingCards: true, isShowingTrivia: false}
+  }
+
+  onClickCardsHandler() {
+    if (!this.state.isShowingCards) {
+      this.setState({isShowingCards: true, isShowingTrivia: false});
+    }
+  }
+
+  onClickTriviaHandler() {
+    if (!this.state.isShowingTrivia) {
+      this.setState({isShowingCards: false, isShowingTrivia: true});
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header isShowingCards={this.state.isShowingCards} isShowingTrivia={this.state.isShowingTrivia} onClickCards={this.onClickCardsHandler.bind(this)} onClickTrivia={this.onClickTriviaHandler.bind(this)}/>
+      </div>);
+  }
 }
 
 export default App;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Header.css';
 
-function Header(props) {
+const Header = ({ isShowingCards, isShowingTrivia, onClickCards, onClickTrivia }) => {
   return (
     <div>
       <div className="Header">
         <span className="Header-title">Spanish Padawan</span>
+        <span>
+          <button className={isShowingCards ? "Header-button selected" : "Header-button"} onClick={onClickCards}>Cards</button>
+          <button className={isShowingTrivia ? "Header-button selected" : "Header-button"} onClick={onClickTrivia}>Trivia</button>
+        </span>
       </div>
       <div className="Header-divider"></div>
     </div>);
